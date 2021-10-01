d1bea1b1430e005a8088a05fd581d65bbcae31bd
phuxiteo3 <74146862+phuxiteo3@users.noreply.github.com>
2021-10-01 09:20:34 +0700

    Add files via upload

---
+++ src/demo/SecondClass.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo;
+
+import java.util.List;
+
+/**
+ *
+ * @author Dell
+ */
+public class SecondClass {
+
+    public static void main(String[] args) {
+        int a, b;
+        a = 3;
+        b = 5;
+        int result1 = a + b;
+        int result2 = a - b;
+        int result3 = a * b;
+        int result4 = a / b;
+
+        System.out.println(result1);
+    }
+}
